34cac443235ca50c5494c24a552504035dda88a3
Tiffany Liu <tiffany_l_liu@trendmicro.com>
2024-06-30 20:02:58 +0800

    feat: create card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,8 @@
 
-const Card = (session: any) => {
+const Card = () => {
   return (
-    <div className="w-[345px] h-[198px] bg-pink-400">
+    <div className="w-[345px] h-[198px] bg-pink-400 rounded flex items-end p-3">
+      <div className="text-3xl font-bold">Artical Title</div>
     </div>
   )
 }
