293926c4a2ca688a072e703efc65922da6c7b205
Tiffany Liu <tiffany_l_liu@trendmicro.com>
2024-06-30 20:04:09 +0800

    config: setup github auth

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const {
     handlers: { GET, POST },
@@ -11,6 +12,17 @@ export const {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
             authorization: {
                 params: {
                     prompt: "consent",
